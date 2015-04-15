3eae105a4eb805b8d1c6d4240f1051ff2611bc61
笔迹 <yesicoo@163.com>
2015-04-15 23:32:32 +0800

    项目创建

---
+++ WebSocketLib/WebSocketLib.Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebSocketLib.Test
+{
+    class Program
+    {
+      static  WsMain wsMain = null;
+        static void Main(string[] args)
+        {
+            wsMain = new WsMain(10801);
+            wsMain.RegisterCommItem("Hello", DoHello);
+            Console.ReadLine();
+        }
+
+        private static void DoHello(string commKey, Guid guid, Dictionary<string, string> keyValues)
+        {
+            Console.WriteLine(string.Format("CommKey:{0};Guid{1};Key1:{2}", commKey, guid, keyValues["Key1"]));
+            wsMain.SendMessage(guid, "SB", keyValues);
+        }
+    }
+}
---
+++ WebSocketLib/WebSocketLib/WsMain.cs
@@ -0,0 +1,111 @@
+﻿using Fleck;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace WebSocketLib
+{
+    public class WsMain
+    {
+        ConcurrentDictionary<Guid, IWebSocketConnection> Wscs = new ConcurrentDictionary<Guid, IWebSocketConnection>();
+        ConcurrentDictionary<string, Action<string, Guid, Dictionary<string, string>>> CommItems = new ConcurrentDictionary<string, Action<string, Guid, Dictionary<string, string>>>();
+        WebSocketServer WSS = null;
+
+        public WsMain(int port)
+        {
+            WSS = new WebSocketServer("ws://0.0.0.0:" + port);
+        }
+
+        public void Start()
+        {
+            WSS.Start(socket =>
+              {
+                  socket.OnOpen = () =>
+                  {
+                      Wscs.TryAdd(socket.ConnectionInfo.Id, socket);
+                  };
+                  socket.OnClose = () =>
+                  {
+                      IWebSocketConnection so = null;
+                      Wscs.TryRemove(socket.ConnectionInfo.Id, out so);
+                  };
+                  socket.OnMessage = (msg) => DoMessage(socket, msg);
+                  socket.OnBinary = (bs) => DoBinary(socket, bs);
+                  socket.OnError = (e) => DoError(socket, e);
+              });
+
+        }
+
+        private void DoError(IWebSocketConnection socket, Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        public bool RegisterCommItem(string CommKey, Action<string, Guid, Dictionary<string, string>> doAction)
+        {
+            return CommItems.TryAdd(CommKey, doAction);
+        }
+
+        public bool UnRegisterCommItem(string CommKey)
+        {
+            Action<string, Guid, Dictionary<string, string>> doAction = null;
+            return CommItems.TryRemove(CommKey, out doAction);
+        }
+
+
+        private object DoBinary(IWebSocketConnection socket, byte[] bs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoMessage(IWebSocketConnection socket, string msg)
+        {
+
+            dynamic result = JsonConvert.DeserializeObject(msg);
+            string CommKey = result.CommKey;
+            Dictionary<string, string> KeyValues = result.KeyValue;
+            Action<string, Guid, Dictionary<string, string>> doAction = null;
+            if (CommItems.TryGetValue(CommKey, out doAction))
+            {
+                new Thread(() => { doAction(CommKey, socket.ConnectionInfo.Id, KeyValues); }).Start();
+            }
+            else
+            {
+
+            }
+        }
+
+        public bool SendMessage(Guid guid, string commKey, Dictionary<string, string> keyValues)
+        {
+
+            IWebSocketConnection socket = null;
+            if (Wscs.TryGetValue(guid, out socket))
+            {
+                dynamic SendData = new ExpandoObject();
+                SendData.CommKey = commKey;
+                SendData.KeyValues = keyValues;
+                string strData = JsonConvert.SerializeObject(SendData);
+                if (socket.IsAvailable)
+                {
+                    socket.Send(strData);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+    }
+}
